64ad374bb09a5b3eeca8ac191050d0d12d755a3a
Jithin Jose <jithinjose165@yahoo.com>
2024-08-17 18:14:43 +0530

    First commit

---
+++ src/main/java/com/jithinjosects/ecart/EcartApplication.java
@@ -0,0 +1,13 @@
+package com.jithinjosects.ecart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jithinjosects/ecart/EcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jithinjosects.ecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
